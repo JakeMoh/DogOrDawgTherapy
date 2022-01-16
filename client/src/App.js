ae741fe5069b9dde16fd9b2ac49c97c57e3fa15c
Jake <jakem5531@gmail.com>
2022-01-15 17:16:36 -0800

    App router

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import OpenAI from 'openai-api';
+import { Link, Route, BrowserRouter, Routes } from 'react-router-dom';
+import LandingPage from './component/landingPage'
+import Dog from './component/dog'
+import Dawg from './component/dawg'
+
+const OPENAI_API_KEY = "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";
+
+const openai = new OpenAI(OPENAI_API_KEY);
+
+// (async () => {
+//   const gptResponse = await openai.complete({
+//     engine:"davinci",
+//     prompt:"You: What have you been up to?\nFriend: Watching old movies.\nYou: Did you watch anything interesting?\nFriend:",
+//     temperature:0.4,
+//     max_tokens:60,
+//     top_p:1.0,
+//     frequency_penalty:0.5,
+//     presence_penalty:0.0,
+//     stop:["You:"]
+//   });
+
+//   console.log(gptResponse.data);
+// })();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Dog go bark bark
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/dog" element={<Dog />} />
+        <Route path="/dawg" element={<Dawg />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/component/dawg.js
@@ -0,0 +1,7 @@
+function Dawg() {
+  return (
+    <h1>Dawg</h1>
+  )
+}
+
+export default Dawg;
